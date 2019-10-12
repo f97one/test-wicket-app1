e1bea5709a2db6eefd10bcc6f433cc0c876f749a
HAJIME Fukuna <f97one@gmail.com>
2019-10-12 09:21:52 +0000

    EJBのインジェクションを追加。

--- src/main/java/net/formula97/webapps/gae/HomePage.java
+++ src/main/java/net/formula97/webapps/gae/HomePage.java
@@ -1,18 +1,23 @@
 package net.formula97.webapps.gae;
 
+import net.formula97.webapps.gae.ejbBean.EnterpriseMessage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
+import javax.ejb.EJB;
+
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private EnterpriseMessage enterpriseMessage;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
+		add(new Label("message", enterpriseMessage.message));
 
 	}
+
 }
