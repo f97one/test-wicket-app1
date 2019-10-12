d8d66d40eaf590d4da5f1bf7475d64ec1fc974d3
HAJIME Fukuna <f97one@gmail.com>
2019-10-12 09:20:20 +0000

    EJBによるManaged Beanとしてpublicなメンバー変数をもつクラスを追加。

---
+++ src/main/java/net/formula97/webapps/gae/ejbBean/EnterpriseMessage.java
@@ -0,0 +1,9 @@
+package net.formula97.webapps.gae.ejbBean;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class EnterpriseMessage {
+
+    public String message = "Welcome to the EJB world!";
+}
