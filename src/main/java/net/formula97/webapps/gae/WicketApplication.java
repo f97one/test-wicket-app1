26e0609fdf499887d46b9d9175104d6170e5990c
HAJIME Fukuna <f97one@gmail.com>
2019-10-12 09:21:10 +0000

    EJBのインジェクションを追加。

--- src/main/java/net/formula97/webapps/gae/WicketApplication.java
+++ src/main/java/net/formula97/webapps/gae/WicketApplication.java
@@ -2,32 +2,29 @@ package net.formula97.webapps.gae;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.wicketstuff.javaee.injection.JavaEEComponentInjector;
 
 /**
  * Application object for your web application.
  * If you want to run this application without deploying, run the Start class.
- * 
+ *
  * @see net.formula97.webapps.gae.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
-		return HomePage.class;
-	}
+public class WicketApplication extends WebApplication {
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+        return HomePage.class;
+    }
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init()
-	{
-		super.init();
-
-		// add your configuration here
-	}
+    /**
+     * @see org.apache.wicket.Application#init()
+     */
+    @Override
+    public void init() {
+        super.init();
+        getComponentInstantiationListeners().add(new JavaEEComponentInjector(this));
+    }
 }
