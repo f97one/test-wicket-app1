45aa9ff25cd9db72fdbbb03c12c34662b0d7d6b6
HAJIME Fukuna <f97one@gmail.com>
2019-10-20 05:36:45 +0000

    Spring Bootが使うエントリーポイントを追加したので、Jetty起動処理を削除。

--- src/main/java/net/formula97/webapps/gae/WicketApplication.java
+++ src/main/java/net/formula97/webapps/gae/WicketApplication.java
@@ -3,6 +3,9 @@ package net.formula97.webapps.gae;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.util.crypt.CharEncoding;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,7 +14,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *
  * @see net.formula97.webapps.gae.Start#main(String[])
  */
+@SpringBootApplication
 public class WicketApplication extends WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WicketApplication.class, args);
+    }
+
     /**
      * @see org.apache.wicket.Application#getHomePage()
      */
@@ -28,9 +37,14 @@ public class WicketApplication extends WebApplication {
         super.init();
 
         // Spring Injection
+        getRequestCycleSettings().setResponseRequestEncoding(CharEncoding.UTF_8);
+        getMarkupSettings().setDefaultMarkupEncoding(CharEncoding.UTF_8);
+
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        //Scan package for annotated beans
         ctx.scan("net.formula97.webapps.gae.ejbBean");
         ctx.refresh();
+
         getComponentInstantiationListeners().add(new SpringComponentInjector(this, ctx));
     }
 }
