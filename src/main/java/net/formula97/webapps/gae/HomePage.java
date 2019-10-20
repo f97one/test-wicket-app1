c4ee41dfc0cde45217dd93d1b1d8899a8787e4a4
HAJIME Fukuna <f97one@gmail.com>
2019-10-20 03:49:55 +0000

    画面にJVMのバージョンを張る処理を追加。

--- src/main/java/net/formula97/webapps/gae/HomePage.java
+++ src/main/java/net/formula97/webapps/gae/HomePage.java
@@ -1,11 +1,13 @@
 package net.formula97.webapps.gae;
 
-import net.formula97.webapps.gae.ejbBean.EnterpriseMessage;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.WebPage;
-
 import javax.ejb.EJB;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import net.formula97.webapps.gae.ejbBean.EnterpriseMessage;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -16,8 +18,8 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("message", enterpriseMessage.message));
-
+		add(new Label("message", Model.of(enterpriseMessage.message)));
+		add(new Label("versionCode", Model.of(enterpriseMessage.versionCode)));
 	}
 
 }
